28abcbe38c1e405741b5e049532c97a3f202ab00
Mauricio Brito dos Santos <164758301+MauAnalyst@users.noreply.github.com>
2024-07-27 10:40:02 -0300

    Update simulador_cdi.js
    alterando o link da api do cdi para que seja um json local

--- simuladores/assets-simuladores/simulador_cdi.js
+++ simuladores/assets-simuladores/simulador_cdi.js
@@ -51,7 +51,7 @@ anoAtualTaxa.forEach(e => e.innerHTML = anoAtual);
 anoAnteriorTaxa.innerHTML = anoAtual -1;
 
 //---- definindo os dados das taxas
-const apiCDI = "http://ipeadata.gov.br/api/odata4/ValoresSerie(SERCODIGO='BM12_TJCDI12')";
+const apiCDI = "../APIs/cdi.json'";
 
 async function buscaDados(){
     try{
